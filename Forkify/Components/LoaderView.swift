cab6f40aa9a61d856da675943420b271f3143af8
Cameron Ramirez <caribcoded@gmail.com>
2023-03-30 11:59:49 -0400

    LoaderView

---
+++ Forkify/Components/LoaderView.swift
@@ -0,0 +1,26 @@
+//
+//  LoaderView.swift
+//  Forkify
+//
+//  Created by Cameron Ramirez on 3/30/23.
+//
+
+import SwiftUI
+
+struct LoaderView: View {
+    var body: some View {
+        VStack {
+            ProgressView()
+                .tint(K.Colors.grayDark1)
+                .scaleEffect(2, anchor: .center)
+        }
+        .padding()
+    }
+}
+
+struct LoaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoaderView()
+            .previewLayout(.sizeThatFits)
+    }
+}
